6e2e06f6b36152d6eaa3d78d12a24b9bee1763fd
PedrovRamos <pvramos.cunha@gmail.com>
2023-06-05 21:35:25 -0300

    Adicionando projeto com req 1 concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,19 @@
+def is_time_invalid(time):
+    if time is None or not isinstance(time, int):
+        return True
+    return False
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui.."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count = 0
+    for time in permanence_period:
+        if is_time_invalid(time[0]) or is_time_invalid(time[1]):
+            return None
+
+        if time[0] <= target_time <= time[1]:
+            count += 1
+
+    return count
