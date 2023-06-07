0565b46afe9697e0c16f77c0bac1bf9e030a8241
PedrovRamos <pvramos.cunha@gmail.com>
2023-06-06 21:56:30 -0300

    Adicionando projeto com 4 req finalizados

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    if type(word) != str or word == "":
+        return False
+    word = word.lower()
+    if word == word[::-1]:
+        return True
+    return False
     raise NotImplementedError
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("vasco", 25) == "ocsav"
+    assert encrypt_message("vasco", 2) == "ocs_av"
+    assert encrypt_message("vasco", 3) == "sav_oc"
+
+    with pytest.raises(TypeError):
+        encrypt_message("vasco", "gigante")
